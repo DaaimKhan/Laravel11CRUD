316b90d7f7ba309043d779d54964f465ccd5a7bd
DaaimKhan <113842455+DaaimKhan@users.noreply.github.com>
2024-10-18 23:08:36 +0530

    tesat

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Create User</title>
+    <title>Edit User</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
@@ -22,42 +22,46 @@
             <div class="col-md-10">
                 <div class="card borde-0 shadow-lg my-4">
                     <div class="card-header bg-dark">
-                        <h3 class="text-white">Create Product</h3>
+                        <h3 class="text-white">Edit Product</h3>
                     </div>
-                    <form enctype="multipart/form-data" action="{{route('products.store')}}" method="post">
+                    <form enctype="multipart/form-data" action="{{route('products.update', $product->id)}}" method="post">
+                        @method('put')
                         @csrf
                         <div class="card-body">
                             <div class="mb-3">
                                 <label for="" class="form-label h5">Name</label>
-                                <input value="{{old('name')}}" type="text" class="@error('name') is-invalid @enderror form-control form-control-lg" placeholder="Name" name="name">
+                                <input value="{{old('name', $product->name)}}" type="text" class="@error('name') is-invalid @enderror form-control form-control-lg" placeholder="Name" name="name">
                                 @error('name')
                                 <p class="invalid-feedback">{{$message}}</p>
                                 @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="" class="form-label h5">Sku</label>
-                                <input value="{{old('sku')}}" type="text" class="@error('sku') is-invalid @enderror form-control form-control-lg" placeholder="Sku" name="sku">
+                                <input value="{{old('sku', $product->sku)}}" type="text" class="@error('sku') is-invalid @enderror form-control form-control-lg" placeholder="Sku" name="sku">
                                 @error('sku')
                                 <p class="invalid-feedback">{{$message}}</p>
                                 @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="" class="form-label h5">Price</label>
-                                <input value="{{old('price')}}" type="text" class="@error('price') is-invalid @enderror form-control form-control-lg" placeholder="Price" name="price">
+                                <input value="{{old('price', $product->price)}}" type="text" class="@error('price') is-invalid @enderror form-control form-control-lg" placeholder="Price" name="price">
                                 @error('price')
                                 <p class="invalid-feedback">{{$message}}</p>
                                 @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="" class="form-label h5">Description</label>
-                                <textarea value="{{old('description')}}" class="form-control" placeholder="Description" name="description" cols="30" rows="5"></textarea>
+                                <textarea value="{{old('description', $product->description)}}" class="form-control" placeholder="Description" name="description" cols="30" rows="5"></textarea>
                             </div>
                             <div class="mb-3">
                                 <label for="" class="form-label h5">Image</label>
                                 <input type="file" class="form-control form-control-lg" placeholder="Image" name="image">
+                                @if($product->image != "")
+                                <img class="w-50 my-3" src="{{asset('uploads/products/'.$product->image)}}" alt="">
+                                @endif
                             </div>
                             <div class="d-grid">
-                                <button class="btn btn-lg btn-primary">Submit</button>
+                                <button class="btn btn-lg btn-primary">Update</button>
                             </div>
                         </div>
                     </form>
